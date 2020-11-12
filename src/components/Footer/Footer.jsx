385f97cd98b84253083abf7a9f820ee9c5fc7e8c
Lucas Zanek :) <lucaszanek94@gmail.com>
2020-11-12 17:52:01 -0300

    asd

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us</FooterLinkTitle>
+              <Link to="/signin">How it works</Link>
+              <Link to="/signin">Testimonials</Link>
+              <Link to="/signin">Carrers</Link>
+              <Link to="/signin">Investors</Link>
+              <Link to="/signin">Terms of services</Link>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us</FooterLinkTitle>
+              <Link to="/signin">How it works</Link>
+              <Link to="/signin">Testimonials</Link>
+              <Link to="/signin">Carrers</Link>
+              <Link to="/signin">Investors</Link>
+              <Link to="/signin">Terms of services</Link>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us</FooterLinkTitle>
+              <Link to="/signin">How it works</Link>
+              <Link to="/signin">Testimonials</Link>
+              <Link to="/signin">Carrers</Link>
+              <Link to="/signin">Investors</Link>
+              <Link to="/signin">Terms of services</Link>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us</FooterLinkTitle>
+              <Link to="/signin">How it works</Link>
+              <Link to="/signin">Testimonials</Link>
+              <Link to="/signin">Carrers</Link>
+              <Link to="/signin">Investors</Link>
+              <Link to="/signin">Terms of services</Link>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+      </FooterWrapper>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
